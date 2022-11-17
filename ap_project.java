0737886287f54308ebd64f8d64d761519aa90f4a
Dom13377 <geegne123@gmail.com>
2022-11-16 23:27:14 -0800

    started project with command line interface

--- ap_project.java
+++ ap_project.java
@@ -1,7 +1,38 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class ap_project {
-    
     public static void main(String[] args){
-        System.out.println("hello world");
+        //Ingredient Array
+        List<String> raw_ingredients = Arrays.asList("ham", "cheese", "lettuce", "bread", "mayo");
+        String ingredients = String.join(", ", raw_ingredients);
+        //Create Scanner Object
+        Scanner scanobject = new Scanner(System.in);
+        //Activated Boolean
+        boolean program_bool = true;
+        while (program_bool == true){
+            System.out.print("Sandwich Builder>> ");
+            String command = scanobject.nextLine();
+            if (command.equals("list")){
+                list_ingredients(ingredients);
+            }
+            else if (command.equals("exit")){
+                program_bool = exit(program_bool);
+                scanobject.close();
+            }
+            else {
+                System.out.println("Invalid Command!");
+            }
+        }
+    }
+
+    public static void list_ingredients(String ingredients){
+        System.out.println(ingredients);
+    }
+
+    public static boolean exit(boolean program_bool){
+        program_bool = false;
+        return program_bool;
     }
 }
